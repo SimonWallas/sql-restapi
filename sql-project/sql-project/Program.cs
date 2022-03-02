08d99a06587c2649647fe311a66ed6c02aba6ef7
SimonWallas <94540750+SimonWallas@users.noreply.github.com>
2022-03-02 12:21:05 +0100

    Test Sql-DataReader Updateted and ready for web Api

---
+++ sql-project/sql-project/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TestProjectWebApi;
+
+
+Console.WriteLine("Hello, World!");
+
+SqlConsole sql = new SqlConsole();
+sql.Main();
---
+++ sql-project/sql-project/SqlConsole.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace TestProjectWebApi
+{
+
+    class SqlConsole
+    {
+        public void Main()
+        {
+            string str = "Data Source=WI3S03\\SQLEXPRESS;Initial Catalog=restDB;"
+                + "Integrated Security=true";
+            ReadOrderData(str);
+        }
+
+        public void ReadOrderData(string connectionString)
+        {
+            string queryString =
+                "SELECT ID, Name FROM dbo.Cars;";
+
+            using (SqlConnection connection =
+                       new SqlConnection(connectionString))
+            {
+                SqlCommand command =
+                    new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                
+                while (reader.Read())
+                {
+                    ReadSingleRow((IDataRecord)reader);
+                }
+
+                
+                reader.Close();
+            }
+        }
+
+        public static void ReadSingleRow(IDataRecord dataRecord)
+        {
+            Console.WriteLine(String.Format("{0}, {1}", dataRecord[0], dataRecord[1]));
+        }
+    }
+}
